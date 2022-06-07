d09f562561af9649da3ade424e376b50bef30639
jwhisnant97 <jwhisnant97@gmail.com>
2022-06-07 14:55:25 -0500

    fix for deprecated "postgres://..." issue in heroku https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre

--- shortener_app/database.py
+++ shortener_app/database.py
@@ -8,7 +8,7 @@ import os
 
 DATABASE_URL = os.environ['DATABASE_URL']
 if DATABASE_URL and DATABASE_URL.startswith("postgres/"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg2://", 1)
 
 conn = psycopg2.connect(DATABASE_URL,sslmode='require')
 
